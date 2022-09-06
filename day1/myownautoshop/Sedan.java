bd487b3be3d46adb60367b532c24ec7158a22d1f
vaishnavi <vaishnavipb2000@gmail.com>
2022-09-06 17:54:03 +0530

    assignment

---
+++ day1/myownautoshop/Sedan.java
@@ -0,0 +1,18 @@
+package com.example.assignment;
+
+public class Sedan extends car {
+	int length;
+	Sedan(int speed,double regularPrice,String color){
+		super(speed, regularPrice, color);
+	}
+	public double getSalePrice(int length ) {
+		
+		if(length > 20)
+			
+			return super.getSalePrice()-5/100;
+		else
+			return super.getSalePrice()-10/100;
+	}
+		
+}
+
